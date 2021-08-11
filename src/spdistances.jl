e63abe396f3187a53a6877efe75a121c683a61f9
Sikorski <sikorski@zib.de>
2021-08-11 17:57:27 +0200

    clean up spdistances.jl
    status: seems to work fine, nothing left to do (maybe some day rethink cache updates)
    is really fast now

--- src/spdistances.jl
+++ src/spdistances.jl
@@ -15,7 +15,206 @@ function sp_mse(x1, x2, cart1, cart2, res1, res2)
 end
 
 sb_overlap(a, b, k, l) = sbv_linear(a,b,k,l)
-#sb_overlap = sbv_old
+
+##
+
+""" calculate the 1-d overlap between regular discretizations of k and j subdivisions """
+function calc_dists(k,l)
+	x = zeros(k,l)
+	for i in 1:k, j in 1:l
+		l1, r1 = 1/k*(i-1), 1/k*i
+		l2, r2 = 1/l*(j-1), 1/l*j
+		ll = max(l1, l2)
+		r = min(r1, r2)
+		x[i,j] = ll<r ? r-ll :
+			(l1<r2 ? Inf : -Inf)
+	end
+	return x
+end
+
+
+### Main routine for overlap computation
+
+sbv_linear_nocache(args...) = sbv_linear(args...; cachetype=NoCache)
+
+""" linear scheme to the recursive operation, traversing all j and matching to an i on the coarsest level """
+function sbv_linear(b1, b2, k, l; cachetype=IndexCache)
+    dists = calc_dists(k, l)
+    p1 = sortperm(collect(eachcol(b1)))
+    p2 = sortperm(collect(eachcol(b2)))
+
+ 	V = spzeros(size(b1, 2), size(b2, 2))
+
+	b1 = b1[:, p1]
+	b2 = b2[:, p2]
+
+	d, n = size(b1)
+	c = cachetype(d)
+
+	for i = 1:n
+		j = start(c, b1[:, i])
+		firstmatch = search!(i, b1, b2, dists, V, j)
+		update!(c, b1[:,i], firstmatch)
+	end
+
+	return V[invperm(p1), invperm(p2)]
+end
+
+
+function search!(i, b1, b2, overlap, V, j)
+	D, M = size(b2)
+	O = zeros(D)
+
+	firstmatch = zeros(Int, D)
+	lm = 0  # depth of match
+
+	l = 1  # current dimesion/level
+
+	while j <= M
+		o = overlap[b1[l,i], b2[l,j]]
+		if o == -Inf
+			l,j = gonext(l, j, b2)
+		elseif o == Inf
+			if lm < l  # only for cache updates
+				lm = l
+				firstmatch[l:end] .= j
+			end
+			l = l - 1
+			if l == 0
+				break
+			end
+			l, j = gonext(l, j, b2)
+		else
+			if lm < l  # only for cache updates
+				lm = l
+				firstmatch[l:end] .= j
+			end
+			O[l] = o
+			if l == D
+				V[i,j] = prod(O)
+				l, j = gonext(l, j, b2)
+			else
+				l = l + 1
+			end
+		end
+	end
+	return firstmatch
+end
+
+
+# go from position j to the next with a different entry in dimensions[1:l]
+function gonext(l, j, b)
+	n = size(b, 2)
+
+	found = false
+
+	for i in j+1:n, d in 1:l
+		if b[d, i] != b[d,j]
+			l = d
+			j = i
+			found = true
+			break
+		end
+	end
+
+	if found == false
+		j = n + 1
+	end
+
+	return l, j
+end
+
+### Caching for faster traversal, remembers the starting j for increasing i
+
+struct IndexCache
+	box::Vector{Int}
+	ind::Vector{Int}
+end
+
+IndexCache(d) = IndexCache(ones(d), ones(d))
+
+function start(c::IndexCache, b)
+	i = findfirst(c.box .!= b)
+	j = isnothing(i) ? c.ind[end] : c.ind[i]
+	return j
+end
+
+function update!(c::IndexCache, b, firstmatch)
+	i = findfirst(c.box .!= b)
+	if !isnothing(i)
+		c.box[i:end] = b[i:end]
+		c.ind[i:end] = firstmatch[i:end]
+	end
+end
+
+struct NoCache end
+
+NoCache(d) = NoCache()
+start(::NoCache, _) = 1
+update!(::NoCache, _, _) = nothing
+
+
+
+
+## Tests
+
+sbv_methods = [sbv_linear, sbv_linear_nocache, sbv_old]
+
+using Random
+
+subsample(n) = shuffle(1:n)[1:rand(1:n)]
+randboxes(n, k, d) = reduce(hcat, unique(eachcol(rand(1:k, d, n))))
+
+function test_23(method=sbv_methods[1])
+	k = 2
+	l = 3
+	b1 = [1 1 2 2; 1 2 1 2]
+	b2 = [1 1 1 2 2 2 3 3 3; 1 2 3 1 2 3 1 2 3]
+
+	truth = [4 2 0 2 1 0 0 0 0
+	         0 2 4 0 1 2 0 0 0
+			 0 0 0 2 1 0 4 2 0
+			 0 0 0 0 1 2 0 2 4] / 36
+
+	v = method(b1, b2, k, l)
+
+	@assert isapprox(v, truth)
+
+	for i=1:100
+		i1 = subsample(size(b1,1))
+		i2 = subsample(size(b2,1))
+		v = method(b1[:,i1], b2[:,i2], k, l)
+		@assert isapprox(v, truth[i1,i2])
+	end
+end
+
+function test_compare(n=100,m=100,k=10,l=5,d=4; method=sbv_linear, ref=sbv_old)
+	b1 = randboxes(n, k, d)
+	b2 = randboxes(m, l, d)
+
+	v1 = method(b1,b2,k,l)
+	v2 = ref(b1,b2,k,l)
+
+	@assert isapprox(v1,v2)
+end
+
+benchmarkdata() = test_data(10000,10000,10,10,5)
+
+function test_data(n=100,m=100,k=10,l=5,d=4)
+	b1 = randboxes(n, k, d)
+	b2 = randboxes(m, l, d)
+	b1, b2, k, l
+end
+
+function test_sbv()
+	for method in sbv_methods
+		test_23(method)
+	end
+	for i=1:100
+		test_compare(rand(10:1000), rand(10:1000), rand(1:15), rand(1:15), rand(1:8))
+	end
+end
+
 
 sbv_old(b1,b2,k,l) = sparse(spdistances(k,l,b1,b2)..., size(b1,2), size(b2,2))
 
@@ -57,23 +256,13 @@ function spdistances(res1, res2, cart1, cart2)
 end
 
 
-##
+# history
+# v1 - the old bruteforce (correct)
+# v2 - wrong with frank
+# v3 - recursive
+# v4 - linear (with 3 subsequent cache versions) (now correct)
 
-""" calculate the 1-d overlap between regular discretizations of k and j subdivisions """
-function calc_dists(k,l)
-	x = zeros(k,l)
-	for i in 1:k, j in 1:l
-		l1, r1 = 1/k*(i-1), 1/k*i
-		l2, r2 = 1/l*(j-1), 1/l*j
-		ll = max(l1, l2)
-		r = min(r1, r2)
-		x[i,j] = ll<r ? r-ll :
-			(l1<r2 ? Inf : -Inf)
-	end
-	return x
-end
-
-##
+#= wrong recursive version
 
 """ recursive overlap calculation """
 function sbv_rec(b1, b2, k, l)
@@ -153,253 +342,4 @@ function findint(b, d, x, jstart, jend)
 
 	return found, s, e
 end
-
-##
-
-""" linear scheme to the recursive operation, traversing all j and matching to an i on the coarsest level """
-function sbv_linear(b1, b2, k, l; cachetype=IndexCache)
-    dists = calc_dists(k, l)
-    p1 = sortperm(collect(eachcol(b1)))
-    p2 = sortperm(collect(eachcol(b2)))
-
- 	V = spzeros(size(b1, 2), size(b2, 2))
-
-	b1 = b1[:, p1]
-	b2 = b2[:, p2]
-
-	d, n = size(b1)
-	c = cachetype(d)
-
-	for i = 1:n
-		j = start(c, b1[:, i])
-		firstmatch = search!(i, b1, b2, dists, V, j)
-		update!(c, b1[:,i], firstmatch)
-	end
-
-	return V[invperm(p1), invperm(p2)]
-end
-
-sbv_linear_nocache(args...) = sbv_linear(args...; cachetype=NoCache)
-
-struct IndexCache
-	box::Vector{Int}
-	ind::Vector{Int}
-end
-
-IndexCache(d) = IndexCache(ones(d), ones(d))
-
-function start(c::IndexCache, b)
-	i = findfirst(c.box .!= b)
-	j = isnothing(i) ? c.ind[end] : c.ind[i]
-	return j
-end
-
-function update!(c::IndexCache, b, firstmatch)
-	i = findfirst(c.box .!= b)
-	if !isnothing(i)
-		c.box[i:end] = b[i:end]
-		c.ind[i:end] = firstmatch[i:end]
-	end
-end
-
-struct NoCache end
-
-NoCache(d) = NoCache()
-start(::NoCache, _) = 1
-update!(::NoCache, _, _) = nothing
-
-
-function search!(i, b1, b2, overlap, V, j)
-	D, M = size(b2)
-	O = zeros(D)
-	firstmatch = zeros(Int, D)
-	lm = 0  # depth of match
-
-	l = 1  # current dimesion/level
-
-
-	while j <= M
-		o = overlap[b1[l,i], b2[l,j]]
-		if o == -Inf
-			l,j = gonext(l, j, b2)
-		elseif o == Inf
-			if lm < l
-				lm = l
-				firstmatch[l:end] .= j
-			end
-			l = l - 1
-			if l == 0
-				break
-			end
-			l, j = gonext(l, j, b2)
-		else
-			if lm < l
-				lm = l
-				firstmatch[l:end] .= j
-			end
-			O[l] = o
-			if l == D
-				V[i,j] = prod(O)
-				l, j = gonext(l, j, b2)
-			else
-				l = l + 1
-			end
-		end
-	end
-	return firstmatch
-end
-
-
-
-# go from position j to the next with a different entry in dimensions[1:l]
-function gonext(l, j, b)
-	n = size(b, 2)
-
-	found = false
-
-	for i in j+1:n, d in 1:l
-		if b[d, i] != b[d,j]
-			l = d
-			j = i
-			found = true
-			break
-		end
-	end
-
-	if found == false
-		j = n + 1
-	end
-
-	return l, j
-end
-
-updatecache!(c::Nothing, l, j) = begin end
-readresetcache!(c::Nothing, b) = 1
-
-##
-
-sbv_linear_cached(a,b,k,l) = sbv_linear(a,b,k,l, cache(size(a,1)))
-
-mutable struct Cache
-	coords::Vector{Int}
-	inds::Vector{Int}
-	level::Int
-end
-
-cache(D) = Cache(ones(D), ones(D), 0)
-nocache(D) = nothing
-
-function updatecache!(c::Cache, l, j)
-	if c.level < l
-		c.level = l
-		c.inds[l] = j
-	end
-end
-
-function readresetcache!(c::Cache, b)
-	l = 1
-	for i in 1:length(b)
-		if b[i] == c.coords[i]
-			l = i
-		else
-			break
-		end
-	end
-
-	j = c.inds[l]
-	c.coords .= b
-	c.inds[l:end] .= j  # does this change anything? probably...
-	c.level = l
-	return j
-end
-
-
-##
-
-sbv_methods = [sb_overlap ,sbv_old, sbv_rec, sbv_linear, sbv_linear_cached]
-
-function test_23(method=sbv_old)
-	k = 2
-	l = 3
-	b1 = [1 1 2 2; 1 2 1 2]
-	b2 = [1 1 1 2 2 2 3 3 3; 1 2 3 1 2 3 1 2 3]
-
-
-
-	truth = [4 2 0 2 1 0 0 0 0
-	         0 2 4 0 1 2 0 0 0
-			 0 0 0 2 1 0 4 2 0
-			 0 0 0 0 1 2 0 2 4] / 36
-
-	v = method(b1, b2, k, l)
-
-	@assert isapprox(v, truth)
-
-	for i=1:100
-		i1 = subsample(size(b1,1))
-		i2 = subsample(size(b2,1))
-		v = method(b1[:,i1], b2[:,i2], k, l)
-		@assert isapprox(v, truth[i1,i2])
-	end
-end
-
-function test_compare(n=100,m=100,k=10,l=5,d=4; method1=sbv_linear, method2=sbv_old)
-	b1 = randboxes(n, k, d)
-	b2 = randboxes(m, l, d)
-
-	v1 = method1(b1,b2,k,l)
-	v2 = method2(b1,b2,k,l)
-
-    return v1,v2,  isapprox(v1,v2)
-	#@assert
-end
-
-using Random
-Random.seed!(0)
-b1 = randboxes(100, 10, 4)
-b2 = randboxes(100, 5, 4)
-
-#
-#=
-using BenchmarkTools
-
-function benchmark(n, m, k,l,d)
-	for meth in sbv_methods
-		println("method $meth")
-		display(@benchmark test_compare($n, $m, $k, $l, $d, method=$meth))
-	end
-end
 =#
-
-function test_data(n=100,m=100,k=10,l=5,d=4)
-	b1 = randboxes(n, k, d)
-	b2 = randboxes(m, l, d)
-	b1, b2, k, l
-end
-
-function test_sbv()
-	for method in sbv_methods
-		try
-			test_23(method)
-		catch
-			println("method $method failed 23")
-		end
-	end
-
-	for method in sbv_methods
-		for i=1:10
-		try
-
-				v1,v2,same = test_compare(method=method)
-				@assert same
-
-		catch
-			println("method $method failed test_compare")
-		end
-	end
-	end
-end
-
-subsample(n) = shuffle(1:n)[1:rand(1:n)]
-
-randboxes(n, k, d) = reduce(hcat, unique(eachcol(rand(1:k, d, n))))
