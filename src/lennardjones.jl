af4ee400225b7d16b4dd1301fea8d77d1b49b4a2
Sikorski <sikorski@zib.de>
2021-08-26 18:58:50 +0200

    .. refactor, now with tests showing we get the same results with the new api

--- src/lennardjones.jl
+++ src/lennardjones.jl
@@ -1,287 +1,179 @@
-using Base: @locals, NamedTuple, Integer
-using StatsBase
-using Plots
-using LinearAlgebra
-using Statistics
-using Parameters
-using SparseArrays
-import Base.run
-import IterativeSolvers
-using Random
-using JLD2
-using Memoize
-
-export run, run_parallel, Simulation, SpBoxDiscretisation, discretize, committor
+export Simulation, run
 
 
-# batch code
-function runbatch()
-	with_perma(true, true) do
-		batch(Simulation(nsteps=1_000_000), seeds=1:1, levels=vcat(3:20, 30, 40))
-		batch(Simulation(nsteps=10_000_000), seeds=1:1, levels=vcat(3:20, 30, 40))
-		batch(Simulation(nsteps=100_000_000), seeds=1:1, levels=vcat(3:20, 30, 40))
-		batch(Simulation(nsteps=1_000_000_000), seeds=1:1, levels=vcat(3:20, 30, 40))
-	end
+const x0gen =  [0.19920158482463968
+0.13789462153196408
+-0.1709575705426315
+0.0784533378749835
+0.06778720715969005
+-0.2112155752270007]
+
+
+### Lennard Jones specifics
+
+function lennard_jones_harmonic(x; sigma=1/4, epsilon=1, harm=1)
+    x = reshape(x, 2, 3)
+    u = 0.
+    @views for i in 1:3
+        for j in i+1:3
+            r = sigma^2 / sum(abs2, (x[:,i] .- x[:,j]))
+            u += 4*epsilon * (r^6 - r^3)
+        end
+    end
+	u += sum(abs2, x) * harm
+    return u
 end
 
-@memoize PermaDict("cache/batch_") function batch(sim=Simulation(); seeds=1:1, levels=3:14)
-	Random.seed!(0)
-
-	# simulate trajectory
-	sim = Sqra.run_parallel(sim, seeds=seeds)
-
-	# compute discretizations
-	n = length(levels)
-	ds = Array{Any}(undef, n)
-	cs = Array{Any}(undef, n)
-
-
-	Threads.@threads for i in 1:n
-		ncells = levels[i]
-		t1 = @elapsed r = Sqra.discretize(Sqra.SpBoxDiscretisation(ncells=ncells), sim)
-		@info "$t1 seconds for discretization $i"
-		t2 = @elapsed c = Sqra.committor(r)
-		@info "$t2 seconds for committor $i"
-		ds[i] = r
-		cs[i] = c
-	end
-
-
-	errs = Array{Any}(undef, n)
-	Threads.@threads for i in 1:n
-		t = @elapsed errs[i] = Sqra.sp_mse(cs[i], cs[end], ds[i].sb, ds[end].sb)
-		@info "$t seconds for mse $i"
-	end
-
-	return sim, ds, cs, errs
+function ljloop(x, sigma=1/4, epsilon=1., harm=1.)
+    u = 0.
+    @inbounds for i = 0:2
+        for j in i+1:2
+            r = (x[1+2*i] - x[1+2*j]) ^ 2 + (x[2+2*i] - x[2+2*j])^2
+            r = sigma^2 / r
+            u += 4*epsilon * (r^6 - r^3)
+        end
+    end
+    u += sum(abs2, x) * harm
+    return u
 end
 
-### System simulation
-include("system.jl")
+classify(coords::Matrix) = mapslices(classify, coords, dims=1) |> vec
 
+function classify(coords::Vector)
+    ab = coords[3:4] - coords[1:2]
+    ac = coords[5:6] - coords[1:2]
 
-### Discretization
-
-@with_kw struct VoronoiDiscretization
-	prune = Inf
-	npicks = 100
-	neigh = 3*6
-	Q = nothing
-	inds = nothing
-	picks = nothing
-	u = nothing
+    angle = acos(min(dot(ab, ac) / norm(ab) / norm(ac), 1))
+    offset = angle - pi/3  # offset to 60 degree
+    if (abs(offset) < pi/12)  # +- 15 degrees
+        return sign(ab[1]*ac[2] - ab[2]*ac[1])
+    else
+        return 0
+    end
 end
 
-@with_kw struct SpBoxDiscretisation
-	prune = Inf
-	ncells = 6 # in sb
-	boundary = [-ones(6) ones(6)] .* 0.8 # in sb
-	sb = nothing
-	Q = nothing
-	u = nothing
-	picks = nothing
+normalform(x::Matrix) = mapslices(normalform, x, dims=1)
+
+" shift the first particle to 0 and rotate the second onto the x axis"
+function normalform(x)
+    x = reshape(x, 2, div(length(x),2))
+    x = x .- x[:,1]
+
+    one = [1,0]
+    b   = normalize(x[:,2])
+    B   = [b[1] -b[2]
+           b[2]  b[1]]
+    E   = [1 0
+           0 1]
+    A   =  E / B
+    reshape(A * x, length(x))
 end
 
 
-@memoize PermaDict("cache/dis_") function discretize(d::SpBoxDiscretisation, sim::Simulation)
-
-	d.prune < Inf && error("Pruning is not currently supported. Iterative solver should suffice.")
-
-
-	sb = SparseBoxes(sim.x, d.ncells, d.boundary)
-
-	Q, inds = sqra(sb, sim.u, sim.sigma)
-
-	u = sim.u[inds]
-	picks = sim.x[:,inds]
-
-	#=
-	Q, pinds = prune_Q(Q, d.prune)
-	picks = picks[:, pinds]
-	u = u[pinds]
-	sb = prune(sb, pinds)
-	=#
-
-	SpBoxDiscretisation(d, sb=sb, Q=Q, u=u, picks=picks)
+@with_kw struct Simulation
+	x0 = x0gen
+	epsilon = 1
+    r0 = 1/3
+    harm = 1
+    sigma = 1/2
+    dt=0.001
+    nsteps=100000
+    maxdelta=0.1
+	seed = 1
+	x=nothing
+	u=nothing
 end
-
-include("experiment.jl")
-
-
-function trim(d::SpBoxDiscretisation, sim::Simulation, n)
-	sb = trim(d.sb, n)
-	Q, inds = sqra(sb, sim.u, sim.sigma)
-end
-
 
 using Setfield
-function prune(b::SparseBoxes, pinds)
-	b = @set b.boxes = b.boxes[:, pinds]
-	b = @set b.inds = b.inds[pinds]
+
+function permute(s::Simulation)
+	xp = s.x
+	xp = hcat(xp, xp[[4,3,2,1,6,5],:])
+	s = @set s.x = xp
+	s = @set s.u = vcat(s.u, s.u)
+	s = @set s.nsteps = s.nsteps*2
 end
 
+function run_parallel(sim::Simulation; seeds=1:Threads.nthreads())
+	copies=length(seeds)
+	results = Array{Simulation}(undef, copies)
+	batch = cld(sim.nsteps, copies)
 
+	x = Array{Float64}(undef, length(sim.x0), batch * copies)
+	u = Array{Float64}(undef, batch * copies)
 
-# warning: this part is old and should be rewritten as above
-@memoize PermaDict("cache/dis_") function discretize(discretization::VoronoiDiscretization, sim::Simulation)
-	@unpack x, u, sigma = sim
-	@unpack prune = discretization
-	sigma = sim.sigma
-	beta = sigma_to_beta(sigma)
+	Threads.@threads for i in 1:copies
+		results[i] = run(Simulation(sim, nsteps = batch, seed = seeds[i]))
 
-	Q, inds = sqra_voronoi(x, u, discretization.npicks, beta, discretization.neigh)
-
-	Q, pinds = prune_Q(Q, prune)
-	inds = inds[pinds]
-	picks = x[:, inds]
-	u = u[inds]
-
-	discretization = typeof(discretization)(discretization, Q=Q, inds=inds, picks=picks, u=u)
-	return discretization
-end
-
-
-### Committor computation
-
-function changepoints(c)
-	cc = copy(c)
-	for i in 2:length(c)
-		cc[i] != 0 && continue
-		cc[i] = cc[i-1]
+		x[:, (i-1)*batch + 1 : i*batch] = results[i].x[:, 1:batch]
+		u[(i-1)*batch + 1 : i*batch] = results[i].u[1:batch]
 	end
-	findall(diff(cc) .!= 0).+1
+
+	return Sqra.Simulation(sim, x = x, u = u)
+end
+
+@memoize PermaDict("cache/sim_") function run(params::Simulation)
+	@unpack_Simulation params
+
+	Random.seed!(seed)
+	potential(x) = lennard_jones_harmonic(x; epsilon=epsilon, sigma=r0, harm=harm)
+
+	x = eulermaruyama(x0 |> vec, potential, sigma, dt, nsteps, maxdelta=maxdelta,
+		progressbar=Threads.threadid() == 1)
+	u = mapslices(potential, x, dims=1) |> vec
+
+	@pack_Simulation
 end
 
 
 
-#=
-function convergence_error(r::NamedTuple, ns)
-	errors = []
-	for n in ns
-		let u = r.u[1:n],
-			pdist = r.pdist[1:n, 1:n],
-			classes = r.classes[1:n]
+@userplot CloudPlot
 
-			@show size(pdist)
-			A = threshold_adjacency(pdist, r.neigh)
-			Q = sqra(u, A, r.beta)
-			c = solve_committor(Q, classes)[1]
-			push!(errors, mean(abs2, c - r.c[1:n]))
-		end
+""" plotting recipe for plotting multiple cluster states """
+@recipe function f(p::CloudPlot; select=:all, normalize=true, com=nothing)
+	x,  = p.args
+	legend --> false
+	colorbar --> true
+	aspect_ratio --> :equal
+	seriescolor --> :roma
+	#seriescolor --> :hawaii
+	seriesalpha --> 0.3
+
+	#preprocessing
+	n = size(x,2)
+	if isa(select, Integer)
+		i = 1:cld(size(x,2), select):size(x,2)
+	elseif isa(select, AbstractRange) || isa(select, Vector)
+		i = select
+	else
+		i = 1:n
 	end
-	errors
-end
-=#
+	x = x[:,i]
+	normalize && (x = normalform(x))
 
 
+	if isnothing(com)
+		z = [1]
+	else
+		z = com[i]
+	end
 
-
-
-
-
-
-
-
-
-
-### Plotting functions
-#=
-function plot_trajectories(x; kwargs...)
-	scatter!(x[1:2:end,:]', x[2:2:end,:]'; kwargs...)
-end
-
-function plot_triangles!(n; kwargs...)
-	xs = [n[1:2:end,:]; n[[1],:]]
-	ys = [n[2:2:end,:]; n[[2],:]]
-	plot!(xs, ys; kwargs...)
-end
-
-function plot_normalized(x, c)
-	plot_trajectories(normalform(x),alpha=0.5, legend=false)
-	plot_triangles!(normalform(x), alpha=0.3, line_z = c, legend=false, seriescolor=:roma)
-end
-=#
-
-using RecipesBase
-
-function sparsity(d::SpBoxDiscretisation)
-	l = d.ncells
-	dim, n = size(d.picks)
-	sparsity = n / l ^ dim * 100
-end
-
-@recipe function plot(d::SpBoxDiscretisation)
-	title --> "LJ-Cluster, SparseBoxes"
-
-
-	#@series begin
-		l = d.ncells
-		dim, n = size(d.picks)
-		sp = round(sparsity(d), sigdigits=2)
-		annotations := ((.0,-.1), ("l=$l,  n=$n ($sp%)", 8, :black, :left))
-		w, h = round.((d.boundary[1:2,2] .- d.boundary[1:2,1]) / d.ncells, sigdigits=2)
-		xticks --> [-w/2,w/2]
-		yticks --> [-h/2,h/2]
-		CloudPlot((d.picks, ))
-	#=end
 
 	@series begin
-		seriestype := :shape
-		aspect_ratio --> :equal
-		seriesalpha --> 0.1
-		series_color --> :beige
-		legend --> false
-		rectangle(w, h, x, y) = (x .+ [0,w,w,0], y .+ [0,0,h,h])
-		#x, y = minimum(d.picks, dims=2)
-		x, y = -.5, -.5
-		w, h = (d.boundary[1:2,2] .- d.boundary[1:2,1]) / d.ncells
-		rectangle(w,h, x, y)
-	end=#
-end
+		seriestype := :path
 
-
-
-
-
-
-
-#= Graveyard †
-
-function snapshot(v::Vector)
-    x = reshape(v, 2, div(length(v), 2))
-    for i in 1:size(x, 2)
-        scatter!([x[1,i]], [x[2,i]])
+		line_z := (z')
+		n = x
+		xs = [n[1:2:end,:]; n[[1],:]]
+		ys = [n[2:2:end,:]; n[[2],:]]
+		xs, ys
     end
-    xlims!(-1,2)
-    ylims!(-1,2)
+
+	@series begin
+		seriestype := :scatter
+		marker_z := repeat(z, inner=(1,3))
+
+		x[1:2:end,:]', x[2:2:end,:]'
+ 	end
+
 end
-
-macro extract(d)
-    return :(
-        ex=:(); for (k,v) in $d
-           ex = :($ex; try global $k=$v catch end)
-       end; eval(ex); $d
-    )
-end
-
-### Diffusion Maps
-function diffusionmaps(x, n=3; alpha=1,sigma=1)
-	D = cmd.estimation.diffusionmaps.DiffusionMaps(x', sigma, alpha, n=n)
-	return D.dms
-end
-
-
-function pickingdists(dists)
-    d = copy(dists)
-    d[diagind(d)] .= Inf
-    median(minimum(d, dims=2))
-end
-
-function mutual_distances(x::Matrix)
-	d1 = sum(abs2, x[1:2,:] .- x[3:4,:], dims=1)
-	d2 = sum(abs2, x[3:4,:] .- x[5:6,:], dims=1)
-	d3 = sum(abs2, x[5:6,:] .- x[1:2,:], dims=1)
-	[d1; d2; d3]
-end
-
-=#
